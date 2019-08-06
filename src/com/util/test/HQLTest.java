d212088e7b0c722587ba38f93b307e1b2c070d8b
wangning <wangning@jcfc.cn>
2019-08-06 17:57:58 +0800

    calibrate

--- src/com/util/test/HQLTest.java
+++ src/com/util/test/HQLTest.java
@@ -6,9 +6,37 @@ package com.util.test;
  */
 public class HQLTest {
     public static void main(String[] args) {
-        String sql = " select * from LM_PM_SHD where LOAN_NO=? and PS_DUE_DT >= ? and PS_DUE_DT <= ? "
-                + " order by PS_PERD_NO asc";
-        System.out.println(sql);
+        StringBuffer subSql = new StringBuffer(30);
+        subSql.append("select l");
+        subSql.append(" from LmLoan l,LmLoanCont lc  ");
+        subSql.append(" where l.loanNo=lc.loanNo");
+        subSql.append(" and l.loanSts=?");
+        subSql.append(" and ((l.loanDebtSts in (? , ?,?)");
+        subSql.append(" and (");
+        subSql.append(" (l.loanDevaInd='Y' ");
+        subSql.append(" and l.loanDebtSts='NORM'");
+        subSql.append(" and (l.loanDevaOrd is null or l.loanDevaOrd<>'I')");
+        subSql.append(" ) or ");
+        subSql.append(" (l.nextDueDt<=? or l.nextDueDt is null)");// 涓嬫搴旀墸鏃ユ湡
+        subSql.append("))");
+        //保险理赔借据
+        subSql.append(" or (l.claimInd = 'Y' and exists (select 1 from LmClaimLoan cl where cl.relLoanNo = l.loanNo and cl.loanSts = 'ACTV')))");
+        subSql.append(" and l.prcsPageDtInd=?");
+        // 新增加的条件,不再已完成的状态
+        // /不处理已经冻结的账号
+        subSql.append(" and l.loanNo not in (");
+        subSql.append("  select  la.loanNo from LmAtpyDetl la");
+        subSql.append(" where la.atpyValDt=? and la.atpySeqNo=?");
+        subSql.append(" and la.atpySts  in('CP','FZ'))");
+        // sql.append(" and night=?");
+        // 7*24小时还款数据，不再进行批扣
+        subSql.append(" and l.loanNo not in (");
+        subSql.append(" select i.loanNo from IntfLoanTxQue i ");
+        subSql.append(" where i.loanNo=l.loanNo and i.prcsSts='INIT' ");
+        subSql.append(" and i.txTyp='LNC4' and i.txDt=?) ");
+        subSql.append( " and not exists (select 1 from LmAtpyDetlBegin b where b.id.loanNo = l.id.loanNo and b.id.atpyValDt = ?)");
+        subSql.append(" order by l.loanNo");
+        System.out.println(subSql);
     }
 
     public static String getDateIncDay(String inputDate, String dayCount,String operation) {
@@ -17,5 +45,17 @@ public class HQLTest {
                 .append("')").append(operation).append(dayCount).append(",'")
                 .append("yyyy-MM-dd").append("') ");
         return subSql.toString();
+    }
+
+    public static String getDateIncMonth(String inputDate, String monthCount,String operation) {
+        StringBuffer subSql = new StringBuffer();
+        subSql.append("to_char(add_months(to_date(").append(inputDate).append(",'").append("yyyy-MM-dd")
+                .append("'),").append(operation).append(monthCount).append("),'")
+                .append("yyyy-MM-dd").append("') ");
+        return subSql.toString();
+    }
+
+    public static String getNvl() {
+        return "nvl";
     }
 }
--- src/com/util/test/Test.java
+++ src/com/util/test/Test.java
@@ -4,16 +4,34 @@ package com.util.test;
 /**
  * Created by wn on 2017/12/13.
  */
-public class Test{
+public class Test {
 
     public static void main(String[] args) {
-        Thread thread  = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("...");
-            }
-        });
-        thread.setDaemon(true);
-        thread.start();
+        StringBuffer sql = new StringBuffer();
+        sql.append("select l from LmLoan l");
+        sql.append(" where l.loanSts = ? and l.prcsPageDtInd=? and l.loanDebtSts in(?,?,?) ");
+        sql.append(" and (l.atpySts != 'SK' or l.atpySts is null)");
+        sql.append(" and (l.loanOdInd='Y' or l.loanStpAccInd='R')");
+        sql.append(" and ((l.loanDebtSts='NORM' and ");
+        sql.append(" ((l.loanGraceTyp='E' and ");
+        sql.append(getDateIncDay("l.nextDueDt",
+                "l.loanOdGrace", "+"));
+        sql.append(" <=?) ");
+        sql.append(" or ((l.loanGraceTyp='P' or l.loanGraceTyp is null) and l.nextDueDt <= ?))) ");// 转逾期增强
+        sql.append(" or (l.loanDebtSts = 'DELQ' and exists ");
+        sql.append(" (from LmPmShd ps where ps.id.loanNo = l.loanNo ");
+        sql.append(" and ps.prcpState = '10' ");
+        sql.append(" and ps.psDueDt <= ? and ps.id.psPerdNo <> 0 and ps.setlInd='N')))");// 后续期转逾期
+
+        System.out.println(sql);
+    }
+
+    public static String getDateIncDay(String inputDate, String dayCount,String operation) {
+        StringBuffer subSql = new StringBuffer();
+            subSql.append("to_char(to_date(").append(inputDate).append(",'").append("yyyy-MM-dd")
+                    .append("')").append(operation).append(dayCount).append(",'")
+                    .append("yyyy-MM-dd").append("') ");
+        return subSql.toString();
     }
 }
+
