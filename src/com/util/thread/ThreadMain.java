08b992c18724e0c8db73f372fa61ffca191f611d
wn <543238461@qq.com>
2020-05-12 23:47:26 +0800

    JUC各种同步锁

---
+++ src/com/util/thread/ThreadMain.java
@@ -0,0 +1,32 @@
+package com.util.thread;
+
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.StampedLock;
+
+public class ThreadMain {
+    //synchronized
+    //volatile
+    //JUC中同步锁  java.util.concurent
+    ReentrantLock reentrantLock = new ReentrantLock();
+    AtomicInteger atomicInteger = new AtomicInteger();
+    LongAdder longAdder = new LongAdder();
+    CountDownLatch countDownLatch = new CountDownLatch(10);
+    CyclicBarrier cyclicBarrier = new CyclicBarrier(10, new Runnable() {
+        @Override
+        public void run() {
+            System.out.println("满员 发车。。。");
+        }
+    });
+
+    Phaser phaser = new Phaser();
+
+    ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    StampedLock stampedLock = new StampedLock();
+    Semaphore semaphore = new Semaphore(10);
+    Exchanger<String> exchanger = new Exchanger<>();
+}
